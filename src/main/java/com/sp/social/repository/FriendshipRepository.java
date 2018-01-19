95c6e1ed653526dbe6a5aa1af0a099b52ba5f3d9
Yewmun <yewmun.tai@unchartedpartners.com>
2018-01-19 10:51:13 +0800

    removed redundant import

--- src/main/java/com/sp/social/repository/FriendshipRepository.java
+++ src/main/java/com/sp/social/repository/FriendshipRepository.java
@@ -3,8 +3,6 @@ package com.sp.social.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.sp.social.model.Friendship;
 import com.sp.social.model.Person;
