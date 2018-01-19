3eeb4c586669f5e5616d1e7b7e39d59798836370
Yewmun <yewmun.tai@unchartedpartners.com>
2018-01-19 08:41:09 +0800

    Initial commit

---
+++ src/test/java/com/sp/social/SpSocialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sp.social;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpSocialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
